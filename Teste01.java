2c73a76f605aacfd599ec21e45df74fc1dc9c3c9
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2025-08-27 22:39:41 -0300

    Create Teste01.java

---
+++ Teste01.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Teste01 {
+//    public static void main(String[] args) {
+//        // diamond operator
+//        List<Aluno> lista = new ArrayList<>();
+//        lista.add(new Aluno(559031, "Maya", "System Information"));
+//        lista.add(new Aluno(559111, "Grazi", "Prost Intuction"));
+//        lista.add(new Aluno(557945, "Lucas", "Travel Eco Come in door"));
+//        lista.add(new Aluno(559033, "Teacher", "Java In Bora"));
+//        lista.add(new Aluno(559001, "Selmano", "Function Lambida"));
+//
+//        // we use list.sort because the "compare to" that the "natural sort" uses does not specify
+//        // the List object type
+//
+//        lista.sort(Comparator.comparing(Aluno::getNome).thenComparing(Aluno::getRm));
+//
+//        lista.forEach(aluno -> System.out.println(aluno));
+//
+//    }
+}
