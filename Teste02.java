fa839dff47713df853463e0e69108a3f15a66e1a
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2025-09-01 20:56:38 -0300

    Update Teste02.java

--- Teste02.java
+++ Teste02.java
@@ -2,14 +2,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Teste02 {
-    public static void main(String[] args) {
-        Set<Aluno> lista = new HashSet<>();
-        lista.add(new Aluno(559031, "Maya", "System Information"));
-        lista.add(new Aluno(559111, "Grazi", "Prost Intuction"));
-        lista.add(new Aluno(557945, "Lucas", "Travel Eco Come in door"));
-        lista.add(new Aluno(559033, "Teacher", "Java In Bora"));
-        lista.add(new Aluno(559001, "Selmano", "Function Lambida"));
-
-        lista.forEach(aluno -> System.out.println(aluno));
-    }
+//    public static void main(String[] args) {
+//        Set<exercicio02.Aluno> lista = new HashSet<>();
+//        lista.add(new exercicio02.Aluno(559031, "Maya", "System Information"));
+//        lista.add(new exercicio02.Aluno(559111, "Grazi", "Prost Intuction"));
+//        lista.add(new exercicio02.Aluno(557945, "Lucas", "Travel Eco Come in door"));
+//        lista.add(new exercicio02.Aluno(559033, "Teacher", "Java In Bora"));
+//        lista.add(new exercicio02.Aluno(559001, "Selmano", "Function Lambida"));
+//
+//        lista.forEach(aluno -> System.out.println(aluno));
+//    }
 }
