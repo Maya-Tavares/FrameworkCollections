ed5cf74567636cbeb2d22a99400b4b29b1baa532
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2025-09-01 20:56:36 -0300

    Update Teste01.java

--- Teste01.java
+++ Teste01.java
@@ -5,17 +5,17 @@ import java.util.List;
 public class Teste01 {
 //    public static void main(String[] args) {
 //        // diamond operator
-//        List<Aluno> lista = new ArrayList<>();
-//        lista.add(new Aluno(559031, "Maya", "System Information"));
-//        lista.add(new Aluno(559111, "Grazi", "Prost Intuction"));
-//        lista.add(new Aluno(557945, "Lucas", "Travel Eco Come in door"));
-//        lista.add(new Aluno(559033, "Teacher", "Java In Bora"));
-//        lista.add(new Aluno(559001, "Selmano", "Function Lambida"));
+//        List<exercicio02.Aluno> lista = new ArrayList<>();
+//        lista.add(new exercicio02.Aluno(559031, "Maya", "System Information"));
+//        lista.add(new exercicio02.Aluno(559111, "Grazi", "Prost Intuction"));
+//        lista.add(new exercicio02.Aluno(557945, "Lucas", "Travel Eco Come in door"));
+//        lista.add(new exercicio02.Aluno(559033, "Teacher", "Java In Bora"));
+//        lista.add(new exercicio02.Aluno(559001, "Selmano", "Function Lambida"));
 //
 //        // we use list.sort because the "compare to" that the "natural sort" uses does not specify
 //        // the List object type
 //
-//        lista.sort(Comparator.comparing(Aluno::getNome).thenComparing(Aluno::getRm));
+//        lista.sort(Comparator.comparing(exercicio02.Aluno::getNome).thenComparing(exercicio02.Aluno::getRm));
 //
 //        lista.forEach(aluno -> System.out.println(aluno));
 //
